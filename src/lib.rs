dbc77898c22a02399a33aa28b97638bd5ec0f82f
Robert Bennett <rltbennett@icloud.com>
2022-05-14 00:46:14 -0400

    Renamed feature `rtf` to `input-format-rtf` to distinguish from output RTF (which has always existed)

--- src/lib.rs
+++ src/lib.rs
@@ -613,7 +613,7 @@ pub enum InputFormat {
     /// reStructuredText
     Rst,
     /// Rich text format
-    #[cfg(feature = "rtf")]
+    #[cfg(feature = "input-format-rtf")]
     Rtf,
     /// HTML
     Html,
@@ -651,7 +651,7 @@ impl std::fmt::Display for InputFormat {
             MarkdownGithub => write!(fmt, "markdown_github"),
             Commonmark => write!(fmt, "commonmark"),
             Rst => write!(fmt, "rst"),
-            #[cfg(feature = "rtf")]
+            #[cfg(feature = "input-format-rtf")]
             Rtf => write!(fmt, "rtf"),
             Html => write!(fmt, "html"),
             Latex => write!(fmt, "latex"),
